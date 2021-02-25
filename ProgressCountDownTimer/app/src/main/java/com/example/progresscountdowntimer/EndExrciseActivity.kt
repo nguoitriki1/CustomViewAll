3bbc1adaf1933f2f17686e40ed24671a4999e4f2
doc_dt <congdoc19101990@gmail.com>
2021-02-25 15:22:47 +0700

    save 25/02/2021

--- ProgressCountDownTimer/app/src/main/java/com/example/progresscountdowntimer/EndExrciseActivity.kt
+++ ProgressCountDownTimer/app/src/main/java/com/example/progresscountdowntimer/EndExrciseActivity.kt
@@ -3,9 +3,9 @@ package com.example.progresscountdowntimer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class TestActivity : AppCompatActivity() {
+class EndExrciseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test)
+        setContentView(R.layout.activity_end_exrcise)
     }
 }
